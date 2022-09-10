d213bb1471af9a2e221f775fa61912ba2faf2106
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-09-10 10:41:03 +0300

    imported reset style and use it globally

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import { Reset } from "styled-reset";
 import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Reset />
     <App />
   </React.StrictMode>
 );
