52beae80b62378e1d39e900234f76fc95a9cbbb6
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-09-10 12:07:56 +0300

    added the styled component and styled all the svgs and their parent and now they are on top of each other

--- src/Components/ParallaxHeroSection/index.jsx
+++ src/Components/ParallaxHeroSection/index.jsx
@@ -1,14 +1,23 @@
+import styled from "styled-components";
 import BackgroundHero from "./BackgroundHero";
 import ForegroundHero from "./ForegroundHero";
 import MidgroundHero from "./MidgroundHero";
 
+// to make all the svgs sit on top of each other
+const StyledSVGWrapper = styled.div`
+  width: 50vw;
+  height: 30vh;
+  position: relative;
+  border: 1px solid red;
+`;
+
 function ParallaxHero() {
   return (
-    <>
+    <StyledSVGWrapper>
       <BackgroundHero />
       <MidgroundHero />
       <ForegroundHero />
-    </>
+    </StyledSVGWrapper>
   );
 }
 export default ParallaxHero;
