6114718039e5036319baa8cdba62142017d74c36
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-09-10 11:53:45 +0300

    created parallex component and it's children

---
+++ src/Components/ParallaxHeroSection/index.jsx
@@ -0,0 +1,14 @@
+import BackgroundHero from "./BackgroundHero";
+import ForegroundHero from "./ForegroundHero";
+import MidgroundHero from "./MidgroundHero";
+
+function ParallaxHero() {
+  return (
+    <>
+      <BackgroundHero />
+      <MidgroundHero />
+      <ForegroundHero />
+    </>
+  );
+}
+export default ParallaxHero;
