5b20a2e89ecfdd51c8e51999014d77637e85b7de
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-09-10 11:54:38 +0300

    changed the styles and imported components to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,43 @@
+import styled from "styled-components";
+import Logo from "./assets/logo.svg";
+import ParallaxHeroSection from "./Components/ParallaxHeroSection/";
+
+const StyledHeroSection = styled.section`
+  width: 100vw;
+  height: 100vh;
+
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+
+  img {
+    height: 25vmin;
+    pointer-events: none;
+  }
+`;
+
+const StyledTitle = styled.h1`
+  color: black;
+  padding-top: 4rem;
+
+  font-size: 2rem;
+  font-weight: 400;
+
+  text-align: center;
+  letter-spacing: 4px;
+  text-transform: uppercase;
+`;
+
 function App() {
-  return <h1>APP is Working</h1>;
+  return (
+    <StyledHeroSection>
+      <img src={Logo} alt="Logo" />
+      <StyledTitle>Back to smoot and firm skins</StyledTitle>
+      <ParallaxHeroSection />
+    </StyledHeroSection>
+  );
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import { Reset } from "styled-reset";
+import "./index.css";
 import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
