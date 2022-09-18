28a44e040d567cf1441b299c33f8c6040acde0e1
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-09-18 21:01:04 +0300

    added some new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 import Logo from "./assets/logo.svg";
+import ContentSection from "./Components/ContentSection";
+import CrossReveal from "./Components/CrossReveal";
 import ParallaxHeroSection from "./Components/ParallaxHeroSection/";
-
+import FaceOne from "./assets/images/faceOne.png";
+import LandScapeOne from "./assets/images/landscapeOne.png";
 const StyledHeroSection = styled.section`
   width: 100vw;
   height: 100vh;
@@ -33,11 +36,26 @@ const StyledTitle = styled.h1`
 
 function App() {
   return (
-    <StyledHeroSection>
-      <img src={Logo} alt="Logo" />
-      <StyledTitle>Back to smoot and firm skins</StyledTitle>
-      <ParallaxHeroSection />
-    </StyledHeroSection>
+    <>
+      <StyledHeroSection>
+        <img src={Logo} alt="Logo" />
+        <StyledTitle>Back to smoot and firm skins</StyledTitle>
+        <ParallaxHeroSection />
+      </StyledHeroSection>
+      <ContentSection
+        title="Take Care"
+        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex beatae enim temporibus ut ipsum error assumenda, illo quidem non, "
+      />
+      <CrossReveal
+        face={FaceOne}
+        landScape={LandScapeOne}
+        name="Mohamud Abshir"
+        job="Front End Developer"
+        sentanceOne="Only Lunch"
+        sentanceTwo="What We Love"
+        authorName="Jaamac Geedi"
+      />
+    </>
   );
 }
 export default App;
